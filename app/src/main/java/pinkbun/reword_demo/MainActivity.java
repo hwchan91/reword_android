10a3e004652304e93b28f25e1709deee13f25136
hwchan91 <hwchan91@gmail.com>
2018-05-22 23:44:52 +0800

    slight fix in using MyWebViewClient

--- app/src/main/java/pinkbun/reword_demo/MainActivity.java
+++ app/src/main/java/pinkbun/reword_demo/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends FragmentActivity {
         mWebView.getSettings().setUseWideViewPort(true);
         mWebView.getSettings().setLoadWithOverviewMode(true);
 
-        WebViewClient webViewClient = new WebViewClient();
+        WebViewClient webViewClient = new MyWebViewClient();
         mWebView.setWebViewClient(webViewClient);
 
         mWebView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; U;` Android 2.0; en-us; Droid Build/ESD20) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17");
